7ede114af2d0c740f28358dab391cd9d05e0b3d1
30-SalsabilaSyifaAz-Zahra <asalsabilatkjsmeksa25@gmail.com>
2026-09-07 13:14:11 +0700

    while ganjil genap

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -1,15 +1,3 @@
-def cek_ganjil_genap():
-    while True:
-        teks = input('Masukkan angka (atau ketik "keluar" untuk berhenti): ')
-        
-        if teks.lower() == 'keluar':
-            print("Program selesai. Terima kasih!")
-            break
-            
-        try:
-            x = int(teks)
-            print(f'angka {x} adalah genap') if x % 2 == 0 else print(f'angka {x} adalah ganjil')
-        except ValueError:
-            print("Input tidak valid! Harap masukkan angka bulat.")
-
-
+while True:
+    x = int(input('Masukkan angka: '))
+    print(f'angka {x} adalah genap') if x % 2 == 0 else print(f'angka {x} adalah ganjil')
